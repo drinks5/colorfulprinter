ed66bb5509d12dd3a9e77ab3596fd2dc3a86631a
drinks <drinks.huang@hypers.com>
2016-05-25 10:35:56 +0800

    update the version

--- colorfulprinter/__init__.py
+++ colorfulprinter/__init__.py
@@ -2,10 +2,10 @@
 # @Author: linlin
 # @Date:   2016-05-24 16:20:39
 # @Last Modified by:   drinks
-# @Last Modified time: 2016-05-24 23:22:17
+# @Last Modified time: 2016-05-25 10:35:28
 
 from npprint import pprint, PrettyPrinter, pformat, isreadable, isrecursive, saferepr
-from termcorlor import color_write, cprint
+from termcolor import color_write, cprint
 
 __all__ = ("pprint", "pformat", "isreadable", "isrecursive", "saferepr",
            "PrettyPrinter", 'color_write', 'cprint')
@@ -13,3 +13,4 @@ __all__ = ("pprint", "pformat", "isreadable", "isrecursive", "saferepr",
 if __name__ == '__main__':
     pp = PrettyPrinter(indent=4)
     pp.pprint([1, 2, 3, [3, 4, [5, 6, [6,7, [7]]]]])
+    cprint('a', 'red')
